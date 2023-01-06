c31ebd332bacc980459e9678bf728ce85fdfda9e
AlejandroGiner <alejandroginerpaez@gmail.com>
2023-01-06 20:16:38 +0100

    últimos retoques de tablas

--- creación-grupo4.sql
+++ creación-grupo4.sql
@@ -15,9 +15,9 @@ CREATE TABLE jugadores(
     id_jugador      NUMERIC(4)      PRIMARY KEY,
     nombre          VARCHAR(50)     NOT NULL,
     apellido        VARCHAR(50)     NOT NULL,
-    id_genero       NUMERIC(2),<
+    id_genero       NUMERIC(2)      NOT NULL,
     fecha_nacimiento DATE           NOT NULL,
-    id_pais         NUMERIC(3),
+    id_pais         NUMERIC(3)      NOT NULL,
     CONSTRAINT jugadores_fk1 FOREIGN KEY (id_genero) REFERENCES generos(id_genero),
     CONSTRAINT jugadores_fk2 FOREIGN KEY (id_pais) REFERENCES paises(id_pais)
 );
@@ -26,22 +26,20 @@ CREATE TABLE partidos(
     id_partido      NUMERIC(4)      PRIMARY KEY,
     id_jugador1     NUMERIC(4)      NOT NULL,
     id_jugador2     NUMERIC(4)      NOT NULL,
-    id_torneo       NUMERIC(3)      NOT NULL,
     n_seccion       NUMERIC(1)      NOT NULL,
     n_ronda         NUMERIC(1)      NOT NULL,
     fecha           DATE            NOT NULL,
-    CONSTRAINT partidos_fk1 FOREIGN KEY (id_torneo,n_seccion,n_ronda)
-        REFERENCES rondas(id_torneo, n_seccion, n_ronda),
-    UNIQUE (id_torneo,n_seccion,n_ronda,id_jugador1,id_jugador2),
+    UNIQUE (n_seccion,n_ronda,id_jugador1,id_jugador2),
     CONSTRAINT partidos_fk2 FOREIGN KEY (id_jugador1) REFERENCES jugadores(id_jugador),
-    CONSTRAINT partidos_fk3 FOREIGN KEY (id_jugador2) REFERENCES jugadores(id_jugador)
+    CONSTRAINT partidos_fk3 FOREIGN KEY (id_jugador2) REFERENCES jugadores(id_jugador),
+    CHECK ( n_ronda BETWEEN 1 AND IF(n_seccion=0,3,4)),
+    CHECK ( n_seccion BETWEEN 0 AND 8)
 );
 
 CREATE TABLE puntuaciones(
     id_partido      NUMERIC(4),
-    n_set           NUMERIC(1)      CHECK(n_set BETWEEN 1 AND 5),
-    punt_jugador1   NUMERIC(4)      CHECK(punt_jugador1 > 0),
-    punt_jugador2   NUMERIC(4)      CHECK(punt_jugador2 > 0),
-    CONSTRAINT puntuaciones_fk1 FOREIGN KEY (id_partido) REFERENCES partidos(id_partido)
-    CONSTRAINT puntuaciones_pk PRIMARY KEY (id_partido, n_set) REFERENCES partidos(id_partido)
+    n_set           NUMERIC(1)      CHECK ( n_set BETWEEN 1 AND 5 ),
+    punt_jugador1   NUMERIC(1)      NOT NULL CHECK ( punt_jugador1 >= 0),
+    punt_jugador2   NUMERIC(1)      NOT NULL CHECK ( punt_jugador2 >= 0),
+    CONSTRAINT sets_fk1 FOREIGN KEY (id_partido) REFERENCES partidos(id_partido)
 );
